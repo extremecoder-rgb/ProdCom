2aa1fa273dde8a960a11e4aa169f49e378b8e69e
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-07-30 00:12:34 +0530

    both database servers are successfully deployed on docker

--- order_service/migration.ts
+++ order_service/migration.ts
@@ -1,12 +1,12 @@
 import { Pool } from "pg";
-import { DB_URL } from "./src/config";
+import { DATABASE_URL } from "./src/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 
 async function runMigration() {
   try {
     console.log("migration start...");
-    const pool = new Pool({ connectionString: DB_URL });
+    const pool = new Pool({ connectionString: DATABASE_URL });
     const db = drizzle(pool);
     await migrate(db, { migrationsFolder: "./src/db/migrations" });
     console.log("migration was successful!");
